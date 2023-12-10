dcdd630ebca4a716aaef1147755da16b548528d6
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-10 18:24:12 +0600

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Auto on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
