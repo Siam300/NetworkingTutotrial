ea4997f9dc39e08951af87f493cf9e7102a6fd2a
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-25 12:31:16 +0600

    Implemented a Service Class

---
+++ NetworkingTutorial/Core/All Coins/Service/CoinDataService.swift
@@ -0,0 +1,43 @@
+//
+//  CoinDataService.swift
+//  NetworkingTutorial
+//
+//  Created by Auto on 25/12/23.
+//
+
+import Foundation
+
+class CoinDataService {
+    func fetchPrice(coin: String, completion: @escaping(Double) -> Void) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Debug: Faild with error \(error.localizedDescription)")
+                //                self.errorMessage = error.localizedDescription
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                //                self.errorMessage = "Bad HTTP response"
+                return
+            }
+            
+            guard httpResponse.statusCode == 200 else {
+                //                self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
+                return
+            }
+            
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            guard let value = jsonObject[coin] as? [String: Double] else {
+                print("Failed to purse value")
+                return
+            }
+            guard let price = value["usd"] else { return }
+            
+            completion(price)
+        }.resume()
+    }
+}
--- NetworkingTutorial/Core/All Coins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/All Coins/ViewModel/CoinsViewModel.swift
@@ -10,32 +10,19 @@ import Foundation
 class CoinsViewModel: ObservableObject {
     @Published var coin = ""
     @Published var price = ""
+    private let service = CoinDataService()
     
     init() {
-        fetchPrice(coin: "bitcoin", price: "bdt")
+        fetchPrice(coin: "bitcoin")
     }
     
-    func fetchPrice(coin: String, price: String) {
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=\(price)"
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            do {
-                guard let data = data else { return }
-                guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-                
-                guard let value = jsonObject[coin] as? [String: Double] else { return }
-                guard let price = value[price] else { return }
-                
-                DispatchQueue.main.async {
-                    self.coin = coin.capitalized
-                    self.price = "\(price) bdt"
-                }
-            } catch {
-                print("Error: \(error.localizedDescription)")
-                return
+    func fetchPrice(coin: String) {
+        service.fetchPrice(coin: coin) { priceFromService in
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "\(priceFromService)"
             }
-        }.resume()
+        }
     }
 }
 
