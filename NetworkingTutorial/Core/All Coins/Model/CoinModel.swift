1060bf2efc0678652a8a4ec2ab944e90a7cd463b
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-31 12:11:43 +0600

    Implemented Loading Images with AsyncImage.

--- NetworkingTutorial/Core/All Coins/Model/CoinModel.swift
+++ NetworkingTutorial/Core/All Coins/Model/CoinModel.swift
@@ -13,9 +13,10 @@ struct CoinModel: Identifiable, Codable, Hashable {
     let symbol: String
     let currentPrice: Double
     let marketCapRank: Int
+    let image: String
     
     enum CodingKeys: String, CodingKey {
-        case id, name, symbol
+        case id, name, symbol, image
         case currentPrice = "current_price"
         case marketCapRank = "market_cap_rank"
     }
