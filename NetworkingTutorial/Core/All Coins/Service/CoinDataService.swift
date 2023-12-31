7b188399c305639e5b444d87f5bbb183cb10b2df
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-31 10:19:29 +0600

    Implemented pagination.

--- NetworkingTutorial/Core/All Coins/Service/CoinDataService.swift
+++ NetworkingTutorial/Core/All Coins/Service/CoinDataService.swift
@@ -14,9 +14,14 @@ protocol CoinServiceProtocol {
 
 class CoinDataService: CoinServiceProtocol, HTTPDataDownloader {
     
+    private var page = 0
+    private let fetchLimit = 30
+    
     //Mark: Async/ Await
     
     func fetchCoins() async throws -> [CoinModel] {
+        page += 1
+
         guard let endpoint = allCoinsURLString else {
             throw CoinAPIError.requestFailed(description: "Invalid API")
         }
@@ -58,8 +63,8 @@ class CoinDataService: CoinServiceProtocol, HTTPDataDownloader {
         components.queryItems = [
             .init(name: "vs_currency", value: "usd"),
             .init(name: "order", value: "market_cap_desc"),
-            .init(name: "per_page", value: "100"),
-            .init(name: "page", value: "1"),
+            .init(name: "per_page", value: "\(fetchLimit)"),
+            .init(name: "page", value: "\(page)"),
             .init(name: "price_change_percentage", value: "24h")
         ]
         
--- NetworkingTutorial/Core/All Coins/View/ContentView.swift
+++ NetworkingTutorial/Core/All Coins/View/ContentView.swift
@@ -35,6 +35,11 @@ struct ContentView: View {
                                 
                             }
                         }
+                        .onAppear {
+                            if coin == viewModel.coins.last {
+                                Task { await viewModel.fetchCoins() }
+                            }
+                        }
                         .font(.footnote)
                     }
                 }
